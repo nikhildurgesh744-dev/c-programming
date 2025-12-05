48efef41bdfca9f9ad4a5629d1272073f2aa5d0c
nikhildurgesh744-dev <nikhildurgesh744@gmail.com>
2025-12-05 14:49:54 +0530

    Add files via upload

---
+++ 05 Calculate the are of circle.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main()
+{
+	int area,radius;
+	printf("enter radius of circle  ");
+	scanf("%d",&radius);
+	area=3.14*radius*radius;
+	printf("\n area of circle is %d",area);
+			
+}
